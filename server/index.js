c3cb32c3703f4943581e93eb11601746eba743f6
Rashid <rashidmubasshir@gmail.com>
2026-07-17 20:10:30 +0530

    connect to MongoDB with Mongoose

--- server/index.js
+++ server/index.js
@@ -1,10 +1,11 @@
 require('dotenv').config();
 const express = require('express');
+const connectDB = require('./config/db');
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-
+connectDB();
 
 app.get('/', (req, res) => {
     res.send(`NearAIIMS API is running`)
