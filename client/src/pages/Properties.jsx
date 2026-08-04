d199f5087232ba17dbd022342f818c3ef262c4b6
Rashid <rashidmubasshir@gmail.com>
2026-08-04 17:18:57 +0530

    feat : connect frontend to backend API with axios, fetch real property data

--- client/src/pages/Properties.jsx
+++ client/src/pages/Properties.jsx
@@ -1,12 +1,27 @@
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 import PropertyCard from '../PropertyCard.jsx';
 
 function Properties() {
+
+  const [properties, setProperties] = useState([]);
+
+  useEffect(() => {
+    axios.get('http://localhost:5000/api/properties')
+      .then((response) => {
+        setProperties(response.data);
+      })
+      .catch((error) => {
+        console.error('Failed to fetch properties:', error);
+      });
+  }, []);
+
   return (
     <div>
       <h1>Available Properties</h1>
-      <PropertyCard />
-      <PropertyCard />
-      <PropertyCard />
+      {properties.map((property) => (
+        <PropertyCard key={property._id} />
+      ))}
     </div>
   );
 }
--- server/index.js
+++ server/index.js
@@ -7,10 +7,16 @@ const propertyRoutes = require('./routes/propertyRoutes');
 const path = require('path');
 const bookingRoutes = require('./routes/bookingRoutes')
 const errorHandler = require('./middleware/errorHandler');
+const cors = require('cors');
+
 
 const app = express();
 app.use(express.json());
 app.use(cookieParser());
+app.use(cors({
+  origin: 'http://localhost:5173', // only for front end requests
+  credentials: true, // allow cookies to be sent with requests
+}));
 app.use('/api/auth', authRoutes);
 app.use('/api/properties', propertyRoutes);
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
